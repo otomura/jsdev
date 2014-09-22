198c11731fda07affe5dcab10019cb2b533b26a2
Otomura Kazutoshi <otomura.kazutoshi@gmail.com>
2014-09-23 01:23:47 +0900

    イベントを受けるところまで。どのリストがクリックされたか検出する方法が不明。

--- js/side-bar.js
+++ js/side-bar.js
@@ -4,8 +4,17 @@ MyApp.Views.SideBar = Backbone.View.extend({
   
   tmpl: _.template($("#sidebar_template").html()),
 
+  events:{
+  	'click' : 'clicked',
+  },
+  
   initialize: function() {
     this.$el.html(this.tmpl());
+  },
+  
+  clicked : function(event){
+      event.preventDefault();
+      console.log('hello ' + $(event.currentTarget).data("id"));
   }
 
 });
